4bb7d569f3244e808b727096a3b99d7019653f29
Jess Tucker <jess.tucker@gmail.com>
2018-05-22 15:49:55 -0700

    Ch. 2 updates

--- ch2/script.js
+++ ch2/script.js
@@ -2,27 +2,57 @@ new Vue({
 	el: '#notebook',
 	data() {
 		return {
-			content: 'This is a note.',
+			notes: JSON.parse(localStorage.getItem('notes')) || [],
+			selectedId: localStorage.getItem('selected-id') || null,
 		}
 	},
 	computed: {
+		selectedNote () {
+			return this.notes.find(note => note.id === this.selectedId)
+		},
 		notePreview () {
-			return marked(this.content)
+			return this.selectedNote ? marked(this.selectedNote.content) : ''
 		},
 	},
+
 	watch: {
-		content: 'saveNote',
+		notes: {
+			handler: 'saveNotes',
+			deep: true,
+		},
+		selectedId (val, oldVal) {
+			localStorage.setItem('selected-id', val)
+		}
 	},
 
 	methods: {
-		saveNote () {
-			console.log('Saving note:', this.content)
-			localStorage.setItem('content', this.content)
-			this.reportOperation('saving')
+		saveNotes () {
+			localStorage.setItem('notes', JSON.stringify(this.notes))
+			console.log('Notes saved!', new Date())
 		},
+
 		reportOperation (opName) {
 			console.log('The', opName, 'operation was completed!')
 		},
+
+		addNote () {
+			const time = Date.now()
+			const note = {
+				id: String(time),
+				title: 'New note ' + (this.notes.length + 1),
+				content: '**Hi!** This notebooks is using [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting!',
+				created: time,
+				favorite: false,
+			}
+			this.notes.push(note)
+			this.selectNote(note)
+		},
+
+		selectNote (note) {
+			this.selectedId = note.id
+		},
+
+
 	},
 
 	created () {
